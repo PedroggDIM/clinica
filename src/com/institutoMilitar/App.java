a2b24d5607affb02ba75d8bdfe450055c86ea267
Pedro Gil <pedrogg76@yahoo.es>
2022-12-05 18:32:01 +0100

    Añadidos cambios

--- src/com/institutoMilitar/App.java
+++ src/com/institutoMilitar/App.java
@@ -7,9 +7,8 @@ import com.institutoMilitar.codigo.Humano;
 import com.institutoMilitar.codigo.Perro;
 
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Funcionando");
-		
+	public static void main(String[] args) {		
+				
 		Perro firulais = new Perro(1234, LocalDate.of(2020, 05, 05), "Firulais", "Sin_Raza");
 		Humano humano = new Humano("Pepe", 36);
 		
